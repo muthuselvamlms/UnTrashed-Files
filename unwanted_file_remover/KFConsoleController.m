f7112d0bc0fab700510bc01592e8429abbd8517a
error-iwedplanner <muthuselvamlms@gmail.com>
2015-07-12 12:52:45 +0530

    Basic Version of Unused Files Identifier Plugin

---
+++ unwanted_file_remover/KFConsoleController.m
@@ -0,0 +1,242 @@
+//
+//  KFConsoleController.m
+//  KFCocoaPodsPlugin
+//
+//  Copyright (c) 2013 Rico Becker, KF Interactive
+//
+//  Permission is hereby granted, free of charge, to any person obtaining a copy
+//  of this software and associated documentation files (the "Software"), to deal
+//  in the Software without restriction, including without limitation the rights
+//  to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+//  copies of the Software, and to permit persons to whom the Software is
+//  furnished to do so, subject to the following conditions:
+//
+//  The above copyright notice and this permission notice shall be included in
+//  all copies or substantial portions of the Software.
+//
+//  THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+//  IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+//  FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+//  AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+//  LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+//  OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+//  THE SOFTWARE.
+//
+
+#import "KFConsoleController.h"
+#import "KFConsoleThemeModel.h"
+#import "IDEKit.h"
+#import "AMR_ANSIEscapeHelper.h"
+//#import <DSUnixTask/DSUnixTask.h>
+
+
+@interface KFConsoleController ()
+
+
+@property (nonatomic, strong) NSMutableDictionary *consoleForTask;
+
+@property (nonatomic, strong) AMR_ANSIEscapeHelper *ansiEscapeHelper;
+
+
+@end
+
+
+@implementation KFConsoleController
+
+- (id)init
+{
+    self = [super init];
+    if (self)
+    {
+        _consoleForTask = [NSMutableDictionary new];
+        _ansiEscapeHelper = [[AMR_ANSIEscapeHelper alloc] init];
+    }
+    return self;
+}
+
++(KFConsoleController *)sharedInstance {
+    static KFConsoleController *console;
+    if (console == nil) {
+        console = [[KFConsoleController alloc] init];
+        console.consoleForTask = [NSMutableDictionary new];
+        console.ansiEscapeHelper = [[AMR_ANSIEscapeHelper alloc] init];
+    }
+    return console;
+}
+
+- (KFConsoleThemeModel *)currentConsoleTheme
+{
+    DVTSourceTextView *sourceTextView = [self sourceTextView:[NSApp mainWindow].contentView];
+    DVTFontAndColorTheme *currentTheme = [sourceTextView currentTheme];
+    
+    if (currentTheme != nil)
+    {
+        KFConsoleThemeModel *themeModel = [KFConsoleThemeModel new];
+        themeModel.outputTextColor = [currentTheme consoleDebuggerOutputTextColor];
+        themeModel.outputTextFont = [currentTheme consoleDebuggerOutputTextFont];
+        
+        return themeModel;
+    }
+    else
+    {
+        return nil;
+    }
+}
+
+
+//- (void)logMessage:(id)object printBold:(BOOL)isBold forTask:(DSUnixTask *)task
+//{
+//    if ([object isKindOfClass:[NSString class]])
+//    {
+//        KFConsoleThemeModel *model = [self currentConsoleTheme];
+//        
+//        NSString *cleanString = nil;
+//        if ([[self.ansiEscapeHelper escapeCodesForString:object cleanString:&cleanString] count] > 0)
+//        {
+//            if (model != nil)
+//            {
+//                self.ansiEscapeHelper.defaultStringColor = model.outputTextColor;
+//            }
+//            
+//             NSAttributedString *attributedString = attributedString = [self.ansiEscapeHelper attributedStringWithANSIEscapedString:object];
+//            object = attributedString;
+//        }
+//        else
+//        {
+//            
+//            
+//            if (model != nil)
+//            {
+//                NSDictionary *attributes = @{NSFontAttributeName:model.outputTextFont, NSForegroundColorAttributeName:model.outputTextColor};
+//                NSAttributedString *attributedString = [[NSAttributedString alloc] initWithString:object attributes:attributes];
+//                object = attributedString;
+//            }
+//        }
+//    }
+//    IDEConsoleTextView *console;
+//    if (task == nil)
+//    {
+//        console = [self consoleView:[NSApp mainWindow].contentView];
+//    }
+////    else
+////    {
+////        console = [self.consoleForTask objectForKey:@(task.processIdentifier)];
+////        if (console == nil)
+////        {
+////            console = [self consoleView:[NSApp mainWindow].contentView];
+////            
+////            if (console)
+////            {
+////                [self.consoleForTask setObject:console forKey:@(task.processIdentifier)];
+////            }
+////        }
+////    }
+//
+//    
+//    console.logMode = isBold ? 2 : 1;
+//    
+//    [console insertText:object];
+//    [console insertNewline:self];
+//    
+//    console.logMode = 0;
+//}
+
+- (void)logMessage:(id)object printBold:(BOOL)isBold
+{
+    if ([object isKindOfClass:[NSString class]])
+    {
+        KFConsoleThemeModel *model = [self currentConsoleTheme];
+
+        NSString *cleanString = nil;
+        if ([[self.ansiEscapeHelper escapeCodesForString:object cleanString:&cleanString] count] > 0)
+        {
+            if (model != nil)
+            {
+                self.ansiEscapeHelper.defaultStringColor = model.outputTextColor;
+            }
+
+            NSAttributedString *attributedString = attributedString = [self.ansiEscapeHelper attributedStringWithANSIEscapedString:object];
+            object = attributedString;
+        }
+        else
+        {
+
+
+            if (model != nil)
+            {
+                NSDictionary *attributes = @{NSFontAttributeName:model.outputTextFont, NSForegroundColorAttributeName:model.outputTextColor};
+                NSAttributedString *attributedString = [[NSAttributedString alloc] initWithString:object attributes:attributes];
+                object = attributedString;
+            }
+        }
+    }
+    IDEConsoleTextView *console;
+    console = [self consoleView:[NSApp mainWindow].contentView];
+    console.logMode = isBold ? 2 : 1;
+    @try {
+        [console insertText:object];
+        [console insertNewline:self];
+
+        console.logMode = 0;
+    }
+    @catch(NSException *exception) {
+
+    }
+    @finally {
+
+    }
+//    [console insertText:object];
+//    [console insertNewline:self];
+//    
+//    console.logMode = 0;
+}
+
+//- (void)removeTask:(DSUnixTask *)task
+//{
+////    [self.consoleForTask removeObjectForKey:@(task.processIdentifier)];
+//}
+
+
+
+- (IDEConsoleTextView *)consoleView:(NSView *)parentView
+{
+    for (NSView *view in [parentView subviews])
+    {
+        if ([view isKindOfClass:NSClassFromString(@"IDEConsoleTextView")])
+        {
+            return (IDEConsoleTextView *)view;
+        }
+        else
+        {
+            NSView *childView = [self consoleView:view];
+            if ([childView isKindOfClass:NSClassFromString(@"IDEConsoleTextView")])
+            {
+                return (IDEConsoleTextView *)childView;
+            }
+        }
+    }
+    return nil;
+}
+
+
+- (DVTSourceTextView *)sourceTextView:(NSView *)parentView
+{
+    for (NSView *view in [parentView subviews])
+    {
+        if ([view isKindOfClass:NSClassFromString(@"DVTSourceTextView")])
+        {
+            return (DVTSourceTextView *)view;
+        }
+        else
+        {
+            NSView *childView = [self sourceTextView:view];
+            if ([childView isKindOfClass:NSClassFromString(@"DVTSourceTextView")])
+            {
+                return (DVTSourceTextView *)childView;
+            }
+        }
+    }
+    return nil;
+}
+
+@end
--- unwanted_file_remover/unwanted_file_remover.m
+++ unwanted_file_remover/unwanted_file_remover.m
@@ -7,6 +7,7 @@
 //
 
 #import "unwanted_file_remover.h"
+#import "KFConsoleController.h"
 
 @interface unwanted_file_remover()
 
@@ -43,7 +44,7 @@
     NSMenuItem *menuItem = [[NSApp mainMenu] itemWithTitle:@"Edit"];
     if (menuItem) {
         [[menuItem submenu] addItem:[NSMenuItem separatorItem]];
-        NSMenuItem *actionMenuItem = [[NSMenuItem alloc] initWithTitle:@"Do Action" action:@selector(doMenuAction) keyEquivalent:@""];
+        NSMenuItem *actionMenuItem = [[NSMenuItem alloc] initWithTitle:@"Identify Unused Files" action:@selector(doMenuAction) keyEquivalent:@""];
         //[actionMenuItem setKeyEquivalentModifierMask:NSAlphaShiftKeyMask | NSControlKeyMask];
         [actionMenuItem setTarget:self];
         [[menuItem submenu] addItem:actionMenuItem];
@@ -53,9 +54,83 @@
 // Sample Action, for menu item:
 - (void)doMenuAction
 {
-    NSAlert *alert = [[NSAlert alloc] init];
-    [alert setMessageText:@"Hello, World"];
-    [alert runModal];
+    dispatch_async(dispatch_get_global_queue(DISPATCH_QUEUE_PRIORITY_DEFAULT, 0), ^{
+        [self printmsgOnXcodeConsole:@"Starting"];
+        [self IdentifyUnusedFiles:[self getCurrentWorkspaceDirectory]];
+        [self printmsgOnXcodeConsole:@"end"];
+    });
+
+}
+
+-(void)IdentifyUnusedFiles:(NSString *)workspacePath {
+    NSFileManager *workspaceManager = [NSFileManager defaultManager];
+    NSArray *Project_Folder = [workspaceManager contentsOfDirectoryAtURL:[NSURL fileURLWithPath:workspacePath] includingPropertiesForKeys:[NSArray arrayWithObjects:NSURLNameKey, NSURLIsDirectoryKey, NSURLContentModificationDateKey, nil] options:NSDirectoryEnumerationSkipsHiddenFiles error:nil];
+    if ([Project_Folder count] > 0) {
+        NSString *path;
+        BOOL isDir;
+        NSString *Pbx_Content = [self getContentofPbxFile];
+        for (NSURL *file in Project_Folder) {
+//            NSLog(@"%@",[file path]);
+            isDir = NO;
+            path = [file path];
+            if ([workspaceManager fileExistsAtPath:path isDirectory:&isDir] && isDir) {
+                if ([path hasSuffix:@".framework"] || [path hasSuffix:@".bundle"] || [path hasSuffix:@".xcassets"] || [path hasSuffix:@".xcodeproj"] || [path hasSuffix:@".xcworkspace"]) {
+                    if (![Pbx_Content containsString:[self getFileName:path]]) {
+                        [self printmsgOnXcodeConsole:[NSString stringWithFormat:@"Not Found : %@",path]];
+                    }
+                }
+                else {
+                    [self IdentifyUnusedFiles:path];
+                }
+            }
+            else {
+                //files
+                if (![Pbx_Content containsString:[self getFileName:path]]) {
+                    [self printmsgOnXcodeConsole:[NSString stringWithFormat:@"Not Found : %@",path]];
+                }
+            }
+        }
+    }
+}
+
+-(NSString *)getFileName:(NSString *)path {
+    return [[path componentsSeparatedByString:@"/"] lastObject];
+}
+
+-(void)printmsgOnXcodeConsole:(NSString *)msg {
+    dispatch_async(dispatch_get_main_queue(), ^{
+    [[KFConsoleController sharedInstance] logMessage:msg printBold:NO];
+    });
+}
+
+-(NSString *)getContentofPbxFile {
+    NSFileManager *workspaceManager = [NSFileManager defaultManager];
+    NSArray *Project_Folder = [workspaceManager contentsOfDirectoryAtURL:[NSURL fileURLWithPath:[self getCurrentWorkspaceDirectory]] includingPropertiesForKeys:[NSArray arrayWithObjects:NSURLNameKey, NSURLIsDirectoryKey, NSURLContentModificationDateKey, nil] options:NSDirectoryEnumerationSkipsHiddenFiles error:nil];
+    NSString *filePath;
+    for (NSURL *file in Project_Folder) {
+        if ([[file path] hasSuffix:@".xcodeproj"]) {
+            filePath = [[file path] stringByAppendingFormat:@"/project.pbxproj"];
+            break;
+        }
+    }
+    return [NSString stringWithContentsOfFile:filePath encoding:NSUTF8StringEncoding error:nil];
+}
+
+- (NSString *) getCurrentWorkspaceDirectory
+{
+    NSArray *workspaceWindowControllers = [NSClassFromString(@"IDEWorkspaceWindowController") valueForKey:@"workspaceWindowControllers"];
+
+    id workSpace;
+
+    for (id controller in workspaceWindowControllers) {
+        if ([[controller valueForKey:@"window"] isEqual:[NSApp keyWindow]]) {
+            workSpace = [controller valueForKey:@"_workspace"];
+        }
+    }
+
+    NSString *workspacePath = [[workSpace valueForKey:@"representingFilePath"] valueForKey:@"_pathString"];
+    workspacePath = [workspacePath stringByReplacingOccurrencesOfString:[workspacePath lastPathComponent] withString:@""];
+    return workspacePath;
 }
 
 - (void)dealloc
